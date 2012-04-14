6d24bc115e7a23caed36cca1afc5978955895b76
Manish Puri <manishspuri@gmail.com>
2012-04-14 07:03:21 +0530

    refactored

--- twitter_search_extension.rb
+++ twitter_search_extension.rb
@@ -5,7 +5,7 @@ class TwitterSearchExtension < TwitterSearch::Client
    
     def get_urls_for_hashtags(hashtags=[],n=100)
         raise RuntimeError, "Please supply atleast one hashtag" if hashtags.empty?
-        hashtags.collect!{|tag| "#" + tag unless tag[0,1].eql?('#')}
+        hashtags.collect!{|tag| "#" + tag }
         tweets = query(:q => "#{hashtags.join(' OR ')}", :rpp => n)
         urls = tweets.map{|tweet|  URI.extract(tweet.text,'http') }
         urls.flatten.uniq  
